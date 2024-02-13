74237ace272ed96d066e53772836da247d09b921
Bondeth KHON <bondeth.khon@gmail.com>
2024-02-13 10:44:03 +0700

    updated action research page.

--- resources/views/pages/action_research.blade.php
+++ resources/views/pages/action_research.blade.php
@@ -26,27 +26,6 @@
         </div>
     </div>
     
-    <div class="conainer ">
-        <div class="ms-5 me-2">
-            <h2 class="it-section-title-3 pb-20 mt-30">Welcome to our Action Research!</h2>
-            <p>Action research is a powerful approach to problem-solving and knowledge creation that bridges the gap between theory and practice. It is a dynamic and participatory process that empowers individuals and communities to identify, analyze, and address real-world challenges they face in their professional or personal lives. By engaging in systematic inquiry and taking action, action research enables individuals to bring about meaningful change and improve their practices.</p>
-            <p>Here at <span class="text-primary">https://btec.edu.kh/</span>, we are passionate about promoting action research as a valuable tool for learning, growth, and social transformation. Through this website, we aim to provide you with a comprehensive understanding of action research and its applications across various fields.</p>
-            <hr>
-            <h2 class="it-section-title-2 pb-10 mt-30">Welcome to our Action Research!</h2>
-            <p>Action research is a cyclical process that involves four main stages: planning, acting, observing, and reflecting. It encourages active collaboration between researchers and practitioners, fostering a co-learning environment. By combining research and action, this approach allows individuals to tackle complex problems through a systematic and iterative process.</p>
-            <p>Through action research, practitioners can enhance their understanding of the contextual factors influencing their work, test strategies, evaluate outcomes, and make informed decisions. This iterative process promotes continuous improvement and innovation, as it encourages adaptation and refinement based on real-time feedback.</p>
-            <hr>
-        </div>
-        <div class="m-5 me-2 ">
-            <h2 class="it-section-title-1 pb-10 mt-30">Benefits of Action Research:</h2>
-            <div class="d-flex align-middle h-30 w-50">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-2 h-2" style="width: 200px; height:200px">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
-                </svg>
-                    <p class="align-middle text-start my-auto ps-4"> <strong>Empowerment:</strong> Action research empowers individuals and communities by giving them the tools and agency to address challenges in their own context. It fosters a sense of ownership and autonomy, leading to sustainable solutions.</p>
-                </div>
-            </div>
-        </div>
-    </div>
+    <x-action_research.action_research />
 
 </x-btec-layout>
