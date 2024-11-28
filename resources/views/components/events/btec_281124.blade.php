7c1cc37418cc8fd745ec13ee5d506b5751d35b8a
KHON BONDETH <bondeth.khon@btec.edu.kh>
2024-11-28 15:03:29 +0700

    added new event.

---
+++ resources/views/components/events/btec_281124.blade.php
@@ -0,0 +1,42 @@
+<!-- event-detail -->
+<div class="it-event-details-area pt-120 pb-120">
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class="col-xl-12 col-lg-12">
+                <div class="it-evn-details-wrap">
+                    <h4 class="it-evn-details-title" style="font-family:'Khmer OS Siemreap';line-height:65px;">
+                        មហាវិទ្យាល័យអប់រំវិទ្យាសាស្ត្រសង្គមនៃវិទ្យាស្ថានគរុកោសល្យបាត់ដំបង សហការជាមួយ ស្ថានទូតសហរដ្ឋអាមេរិក នឹងរៀបចំសិក្ខាសាលាស្តីអំពី៖“ការបង្រៀនតាមបែបគំរោង”
+                    </h4>
+                    <div class="postbox__meta"></div>
+
+                    <div class="it-evn-details-text mb-40" style="font-family:'Khmer OS Siemreap';">
+                        <p>សេចក្ដីជូនដំណឹង៖</p>
+                        <p>មហាវិទ្យាល័យអប់រំវិទ្យាសាស្ត្រសង្គមនៃវិទ្យាស្ថានគរុកោសល្យបាត់ដំបង សហការជាមួយ ស្ថានទូតសហរដ្ឋអាមេរិក នឹងរៀបចំសិក្ខាសាលាស្តីអំពី៖“ការបង្រៀនតាមបែបគំរោង”</p>
+                        <p>ដែលនឹងប្រព្រឹត្តិទៅ៖</p>
+                        <p><strong>កាលបរិច្ឆេទៈ</strong> ថ្ងៃ អង្គារ ទី ៣ ខែ ធ្នូ ឆ្នាំ ២០២៤</p>
+                        <p><strong>ទីតាំង:</strong> សាលសហសិក្សាទី ១</p>
+                        <p><strong>ពេលវេលាៈ</strong> ម៉ោង ១:0០ ដល់ ម៉ោង ៥ៈ០០</p>
+                        <p>សិក្ខាសាលានេះនឹងមានការអញ្ជើញចូលរួមជាវាគ្មិនកិត្តិយស <strong>លោកស្រីសាស្ត្រាចារ្យបណ្ឌិត Merrly Kravitz</strong> គ្រូជំនួយស្ម័គ្រចិត្តមកពីស្ថានទូតអាមេរិកប្រចាំកម្ពុជា។</p>
+
+                        <h6 style="margin-top:50px;">Find us on Facebook below:</h6>
+                        <p>
+                            <a href="https://www.facebook.com/TEC.Battambang" target="_blank">
+                                <img src="{{ asset('assets/img/facebook-post.jpg') }}" alt="Click me here" style="width:192px;">
+                            </a>
+                        </p>
+
+                        <hr style="margin-top:50px;">
+                        <p><strong>ភ្ជាប់ទំនាក់ទំនងជាមួយពួកយើង</strong></p>
+                        <p>📞017 448 869/ 096 3 296 997</p>
+                        <p>📩 info@btec.edu.kh</p>
+                        <p><a href="https://btec.edu.kh">🌐 btec.edu.kh</a></p>
+                        <p>Telegram Channel: <a href="https://t.me/btecnews">t.me/btecnews</a></p>
+                        <p>Youtube Channel: <a href="https://www.youtube.com/@BTECMedia">https://www.youtube.com/@BTECMedia</a></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 mt-20"><img src="{{ asset('assets/img/event/btec-us-embassy-workshop-project-based-teaching.jpg') }}"></div>
+        </div>
+    </div>
+</div>
--- resources/views/pages/home/event.blade.php
+++ resources/views/pages/home/event.blade.php
@@ -18,6 +18,32 @@ Our Events
 		<div class="it-event-2-item-box">
 			<div class="it-event-2-item">
 				<div class="it-event-2-thumb fix" style="height:255px;max-height:255px;">
+					<a href="{{ route('pages.events.detail', ['BTEC-Collaborate-with-US-Embassy-on-Workshop-Project-Based-Teaching']) }}">
+						<img src="{{ asset('assets/img/event/btec-us-embassy-workshop-project-based-teaching.jpg') }}" alt=""></a>
+				</div>
+				<div class="it-event-2-content">
+					<h4 class="it-event-2-title">
+						<a href="{{ route('pages.events.detail', ['BTEC-Collaborate-with-US-Embassy-on-Workshop-Project-Based-Teaching']) }}" style="font-family:'Khmer OS Siemreap';">
+							មហាវិទ្យាល័យអប់រំវិទ្យាសាស្ត្រសង្គមនៃវិទ្យាស្ថានគរុកោសល្យបាត់ដំបង សហការជាមួយ ស្ថានទូតសហរដ្ឋអាមេរិក នឹងរៀបចំសិក្ខាសាលាស្តីអំពី៖“ការបង្រៀនតាមបែបគំរោង”
+						</a>
+					</h4>
+					<div class="it-event-2-text" style="height:224px;max-height:224px;overflow:hidden;font-family:'Khmer OS Siemreap';">
+						<p class="mb-0 pb-10">មហាវិទ្យាល័យអប់រំវិទ្យាសាស្ត្រសង្គមនៃវិទ្យាស្ថានគរុកោសល្យបាត់ដំបង សហការជាមួយ ស្ថានទូតសហរដ្ឋអាមេរិក នឹងរៀបចំសិក្ខាសាលាស្តីអំពី៖“ការបង្រៀនតាមបែបគំរោង”
+							កាលបរិច្ឆេទៈថ្ងៃ អង្គារ ទី ៣ ខែ ធ្នូ ឆ្នាំ ២០២៤
+							ទីតាំង: សាលសហសិក្សាទី ១
+							ពេលវេលាៈ ម៉ោង ១:0០ ដល់ ម៉ោង ៥ៈ០០
+							វាគ្មិនកិត្តិយស: លោកស្រីសាស្ត្រាចារ្យបណ្ឌិត Merrly Kravitz</p>
+					</div>
+					<div class="it-event-2-meta"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="col-xl-4 col-lg-6 col-md-6 mb-30">
+		<div class="it-event-2-item-box">
+			<div class="it-event-2-item">
+				<div class="it-event-2-thumb fix" style="height:255px;max-height:255px;">
 					<a href="{{ route('pages.events.detail', ['BTEC-Competency-Test-Academic-Year-2024-2025']) }}">
 						<img src="{{ asset('assets/img/event/btec-competency-test-2024-2025.jpg') }}" alt=""></a>
 				</div>
@@ -261,7 +287,7 @@ Our Events
 		</div>
 	</div>
 
-	<div class="col-xl-4 col-lg-6 col-md-6 mb-30">
+	<?php /* ?><div class="col-xl-4 col-lg-6 col-md-6 mb-30">
 		<div class="it-event-2-item-box">
 			<div class="it-event-2-item">
 				<div class="it-event-2-thumb fix" style="height:255px;max-height:255px;">
@@ -283,7 +309,7 @@ Our Events
 		</div>
 	</div>
 
-	<?php /* ?><div class="col-xl-4 col-lg-6 col-md-6 mb-30">
+	<div class="col-xl-4 col-lg-6 col-md-6 mb-30">
 		<div class="it-event-2-item-box">
 			<div class="it-event-2-item">
 				<div class="it-event-2-thumb fix" style="height:255px;max-height:255px;">
